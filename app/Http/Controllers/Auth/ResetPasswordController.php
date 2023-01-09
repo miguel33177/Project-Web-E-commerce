09126cf83b97b91c2edfd09395c2e2183f8113e2
Bruno <41094@ufp.edu.pt>
2023-01-09 16:49:04 +0000

    commit

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -26,6 +26,6 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    /*protected $redirectTo = RouteServiceProvider::myAccount;*/
+    
      protected $redirectTo = RouteServiceProvider::HOME;
 }
